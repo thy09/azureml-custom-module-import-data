32213e6e7e9c478be5d27b5b2985670c92ef0c25
Heyi Tang <heta@microsoft.com>
2019-07-18 14:36:08 +0800

    Add module: import generic folder

---
+++ import_data_generic_folder/main.py
@@ -0,0 +1,20 @@
+import os
+import fire
+
+from azure.storage.blob import BlockBlobService
+
+
+def import_data_from_blob(account_name, account_key, container, data_folder, output_folder):
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
+    prefix_len = len(data_folder)
+    for blob_path in blob_service.list_blob_names(container, prefix=data_folder):
+        file_name = blob_path[prefix_len+1:]
+        print(file_name)
+        blob_service.get_blob_to_path(container_name=container, blob_name=blob_path, file_path=os.path.join(output_folder, file_name))
+    pass
+
+
+if __name__ == '__main__':
+    fire.Fire(import_data_from_blob)
