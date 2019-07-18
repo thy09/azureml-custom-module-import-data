883898be5e8560d1319ec85df89cb53d3284a05c
Heyi Tang <heta@microsoft.com>
2019-07-18 14:36:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 
 setup(
-    name="import-data-torchvision",
-    version="0.0.1",
-    description="Import data of torchvision",
-    packages=["import_data_torchvision"],
+    name="azureml-custom-module-import-data",
+    version="0.0.2",
+    description="Import data custom modules.",
+    packages=["import_data_torchvision", "import_data_generic_folder"],
     author="Heyi Tang",
     license="MIT",
     include_package_data=True,
